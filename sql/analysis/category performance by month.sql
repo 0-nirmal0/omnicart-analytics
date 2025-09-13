631228adf6cad03ffa1096f401685b038486ef86
0-nirmal0 <misnmylife@gmail.com>
2025-09-13 16:28:03 +0530

    Category Performance (by Month)

---
+++ sql/analysis/category performance by month.sql
@@ -0,0 +1,11 @@
+-- Category Performance (by Month)
+
+SELECT 
+    p.category,
+    SUBSTRING(o.order_date::text, 1, 7) AS month,
+    ROUND(SUM(oi.quantity * oi.unit_price * (1 - COALESCE(oi.discount,0))), 2) AS revenue
+FROM order_items oi
+JOIN products p USING(product_id)
+JOIN orders o USING(order_id)
+GROUP BY p.category, SUBSTRING(o.order_date::text, 1, 7)
+ORDER BY month, revenue DESC;
