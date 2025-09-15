a730de806d052cc7b9751aa193a480d11672dd1b
0-nirmal0 <misnmylife@gmail.com>
2025-09-15 14:44:28 +0530

    Final Insights

--- sql/analysis/returns analysis.sql
+++ sql/analysis/returns analysis.sql
@@ -7,3 +7,35 @@ SELECT
 FROM returns
 GROUP BY reason
 ORDER BY total_returns DESC;
+
+-- Return Rate
+-- By Orders
+SELECT 
+    ROUND(
+      COUNT(DISTINCT r.order_item_id)::numeric 
+      / COUNT(DISTINCT oi.order_id), 2
+    ) * 100 AS return_rate_percent
+FROM order_items oi
+LEFT JOIN returns r ON oi.order_item_id = r.order_item_id;
+-- 14% of orders had at least one item returned.
+
+-- By Items
+SELECT 
+    ROUND(
+      COUNT(r.return_id)::numeric 
+      / COUNT(oi.order_item_id), 2
+    ) * 100 AS return_rate_percent
+FROM order_items oi
+LEFT JOIN returns r ON oi.order_item_id = r.order_item_id;
+-- 6% of sold items were returned.
+
+-- By Revenue
+SELECT 
+    ROUND(
+      SUM(r.refund_amount) 
+      / SUM(oi.quantity * oi.unit_price * (1 - COALESCE(oi.discount,0))) * 100,
+      2
+    ) AS return_rate_percent
+FROM order_items oi
+LEFT JOIN returns r ON oi.order_item_id = r.order_item_id;
+-- 4.74% of revenue was refunded due to returns.
